0d8c39e0a9d3b356a53aa338bc0c3b2cae9efaa6
SUKANYA85 <101040935+SUKANYA85@users.noreply.github.com>
2022-03-07 04:46:29 +0530

    Add files via upload

---
+++ hyperbolic.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo(tanh(M_PI_4) . "<br>");
+echo(tanh(0.50) . "<br>");
+echo(tanh(-0.50) . "<br>");
+echo(tanh(5) . "<br>");
+echo(tanh(10) . "<br>");
+echo(tanh(-5) . "<br>");
+echo(tanh(-10));
+?>
+</body>
+</html>
